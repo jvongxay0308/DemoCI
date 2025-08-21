969e62ebc62481a630cc5a0fbec1f8c65edf7ff2
Jalana Vongxay <jvongxay0308@gmail.com>
2025-08-22 01:41:39 +0700

    Remove Insecure skip verify

--- main.go
+++ main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"crypto/tls"
 	"net/http"
 )
 
 func main() {
 	hc := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{
-				InsecureSkipVerify: true,
-			},
-		},
+		Transport: &http.Transport{},
 	}
 
 	hc.Get("https://google.com") // This will skip TLS verification
