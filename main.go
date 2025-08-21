b9de35931b724f632ae9e8bbd2949d46e3e0a6cf
Jalana Vongxay <jvongxay0308@gmail.com>
2025-08-22 01:44:39 +0700

    Errcheck

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"io"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -9,5 +12,11 @@ func main() {
 		Transport: &http.Transport{},
 	}
 
-	hc.Get("https://google.com") // This will skip TLS verification
+	resp, err := hc.Get("https://google.com") // This will skip TLS verification
+	if err != nil {
+		log.Fatalf("Failed to make request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	io.Copy(os.Stdout, resp.Body) // Print the response body to stdout
 }
